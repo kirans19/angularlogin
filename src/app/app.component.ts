871e6c1e720c6f9aa6671ce32a38ffdd08510a28
kirans19 <kiranssureshbabu@gmail.com>
2022-09-29 19:01:09 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'login';
+  username:string=""
+  password:string=""
+
+  username1:string=""
+  password1:string=""
+
+  constructor(private http:HttpClient){
+    
+  }
+  loginUser() {
+    this.http.post('http://127.0.0.1:5000/api/signin',{'username':this.username,'password':this.password}).subscribe((response:any)=>{
+      console.log(response.status)
+  })
+  }
+  signUp(){
+    this.http.post('http://127.0.0.1:5000/api/register-user',{'username':this.username1,'password':this.password1}).subscribe((response:any)=>{
+      console.log(response.status)
+    })
+  }
+    
+   
 }
+
+
